79661a702b6577f448026ff067bde8cd55dc2ddc
OpenControl <dev@open-control.org>
2018-08-16 12:15:48 -0400

    Brew formula update for compliance-masonry version v1.1.5

--- compliance-masonry.rb
+++ compliance-masonry.rb
@@ -1,9 +1,9 @@
 class ComplianceMasonry < Formula
   desc "Compliance Masonry is a command-line interface (CLI) that allows users to construct certification documentation using the OpenControl Schema."
   homepage "https://github.com/opencontrol/compliance-masonry"
-  url "https://github.com/opencontrol/compliance-masonry/releases/download/v1.1.4/compliance-masonry_1.1.4_darwin_amd64.tar.gz"
-  version "1.1.4"
-  sha256 "a60c708d750f4098c8bdd233dd1598f3e3503c010b5cf4bf6b10e25dd48f4ed4"
+  url "https://github.com/opencontrol/compliance-masonry/releases/download/v1.1.5/compliance-masonry_1.1.5_darwin_amd64.tar.gz"
+  version "1.1.5"
+  sha256 "d832cbdc83060dc23e59a8a70fafefc3ee3f6a32f7e6f8b5900657d14a889219"
 
   def install
     bin.install "compliance-masonry", "masonry"
