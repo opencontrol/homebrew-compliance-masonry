b68f28495df0c6ab6ebb4717fd4ee3a4666976d7
OpenControl <dev@open-control.org>
2019-03-11 19:35:47 -0400

    Brew formula update for compliance-masonry version v1.2.0

--- compliance-masonry.rb
+++ compliance-masonry.rb
@@ -4,7 +4,7 @@ class ComplianceMasonry < Formula
   homepage "https://github.com/opencontrol/compliance-masonry"
   url "https://github.com/opencontrol/compliance-masonry/releases/download/v1.2.0/compliance-masonry_1.2.0_darwin_amd64.tar.gz"
   version "1.2.0"
-  sha256 "41687850090eadca37539bb6c310ab5fefaa3bd8a6dce76f60cc90521f48c56f"
+  sha256 "57ed0c9c4626a749bc59030c0749d5ae01cdc62e2566db57c137d5eeda1103a2"
 
   def install
     bin.install "compliance-masonry", "masonry"
